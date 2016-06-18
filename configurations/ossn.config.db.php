7d87a53e0c9299627e4a42b95c14a1dc506c99fa
microchat <ender@msn.com>
2016-06-18 19:43:42 +0000

    Add files via upload

---
+++ configurations/ossn.config.db.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.org>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+
+
+// replace localhost with your database host name;
+$Ossn->host = 'localhost';
+
+// replace root with your database username;
+$Ossn->user = 'root';
+
+// replace usbw with your database password;
+$Ossn->password = 'usbw';
+
+// replace ossn with your database name;
+$Ossn->database = 'ossn';
---
+++ configurations/ossn.config.site.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.org>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+
+$Ossn->url = 'http://localhost:8080/ossn/';
+$Ossn->userdata = 'C:/Users/ender/Documents/Web Server/';
+
